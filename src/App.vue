2f2414e65373fab6e3c56242fe99854cb3f1a2b0
苏苏 <d131415000@163.com>
2023-08-13 13:51:10 +0800

    :lipstick:Introducing global fonts

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,21 @@ export default {
 </script>
 
 <style lang="less" >
+@font-face {
+  font-family: '钢笔楷书';
+  src: url('./assets/font/北风钢笔楷书.ttf');
+}
+@font-face {
+  font-family: '方正';
+  src: url('./assets/font/方正字迹-颜世举隶书体\ 简.TTF');
+}
 html,
 body {
   margin: 0;
   padding: 0;
   background-color: var(--bgc);
+  font-family: '钢笔楷书';
+  font-size: 24px;
 }
 
 body::-webkit-scrollbar {
