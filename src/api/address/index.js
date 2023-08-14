2f90691b8aca5a5e056babd36bf7b84a249c8efb
苏苏 <d131415000@163.com>
2023-08-14 14:50:11 +0800

    :alien:

--- src/api/address/index.js
+++ src/api/address/index.js
@@ -2,10 +2,10 @@ import axios from '@/utils/request'
 
 const address = {
     // 1. 获取所有
-    getAddress: () => axios({
-      url: '/ip',
-      method: 'get'
-    }),
+    // getAddress: () => axios({
+    //   url: '/ip',
+    //   method: 'get'
+    // }),
     getVisitorInfo:()=>axios({
       url:'/ip/visitorInfo',
       method:'post'
--- src/api/weather/index.js
+++ src/api/weather/index.js
@@ -1,13 +1,13 @@
 import axios from '@/utils/request' 
 
 const weather = {
-    getWeather: (city) => axios({
+    getWeather: () => axios({
         url: '/weather',
         method: 'post',
         headers: {
           'Content-Type': 'application/json' // 添加Content-Type请求头
         },
-        data: JSON.stringify(city) // 将city对象转换为JSON字符串作为请求体
+        // data: JSON.stringify(city) // 将city对象转换为JSON字符串作为请求体
       }),
 }
 
