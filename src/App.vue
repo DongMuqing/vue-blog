daf4eedb63d9b157fb58cf775aaed6ece1a2d02b
苏苏 <d131415000@163.com>
2023-08-15 13:25:43 +0800

    :fire:字体打包后太大，在服务器运行时资源加载太慢

--- src/App.vue
+++ src/App.vue
@@ -17,20 +17,12 @@ export default {
 </script>
 
 <style lang="less" >
-@font-face {
-  font-family: '钢笔楷书';
-  src: url('./assets/font/北风钢笔楷书.ttf');
-}
-@font-face {
-  font-family: '方正';
-  src: url('./assets/font/方正字迹-颜世举隶书体\ 简.TTF');
-}
 html,
 body {
   margin: 0;
   padding: 0;
   background-color: var(--bgc);
-  font-family: '钢笔楷书';
+  font-family: FangSong;
   font-size: 24px;
 }
 
